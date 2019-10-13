f72a6444e073678fb03ef741a940566b29620441
ferieandriansujarwo <ferie.andrian@bukalapak.com>
2019-10-13 19:08:48 +0700

    fix typo, add new gaz

--- app/function/gaz.js
+++ app/function/gaz.js
@@ -3,12 +3,17 @@ bot.hears(/^ga[sz]$/i, (ctx) => {
 });
 
 bot.hears([/^cot/i, /^bacot l[ou]$/i], (ctx) => {
-  sendReply(ctx, 'Ampun bang :('), 'text';
+  sendReply(ctx, 'Ampun bang :(', 'text');
 });
 
-bot.hears([/^keren/i, /^mantap/i , /^ntap/i], (ctx) => {
+bot.hears([/^keren/i, /^mantap/i, /^ntap/i, /^uwu/i], (ctx) => {
   sendReply(ctx, 'CAADBQADHAADEqpZEMO6Ksi8FJdKFgQ', 'sticker');
   sendReply(ctx, 'Lemes-in aja bang :)', 'text');
+});
+
+bot.hears([/^gagah/i], (ctx) => {
+  sendReply(ctx, 'CAADBQADFwADIr01Eg3MTiqsluZ3FgQ', 'sticker');
+  sendReply(ctx, 'okwokokwowk, Gagah perkasa!!!', 'text');
 });
 
 bot.hears([/^kuy/i, /^yuk/i], (ctx) => {
@@ -22,7 +27,7 @@ bot.hears([/^ijin sick leave/i, /^Ijin sakit/i], (ctx) => {
 function sendReply(ctx, value, type) {
   if (type === 'text') {
     ctx.reply(value);
-  }else if (type === 'sticker') {
+  }else {
     ctx.replyWithSticker(value);
   }
 }
